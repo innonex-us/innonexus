a979812d5ab0307238ec330892dd187c9a504f18
mrokonuzzaman040 <19201103040@cse.bubt.edu.bd>
2024-11-17 23:27:04 +0600

    refactor: comment out CTASection component in page layout

--- app/page.tsx
+++ app/page.tsx
@@ -15,7 +15,7 @@ const page = () => {
       <ServicesOverview />
       <CaseStudiesSnippet />
       <TestimonialsPartners />
-      <CTASection />
+      {/* <CTASection /> */}
     </div>
   );
 };
